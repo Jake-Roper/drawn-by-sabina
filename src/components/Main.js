8b7ef5c73523b2a1a57b93947d84a3e9bb02b7b9
Jake Roper <jakeropergit@outlook.com>
2019-04-01 21:50:11 +0100

    Update information

--- src/components/Main.js
+++ src/components/Main.js
@@ -32,7 +32,7 @@ class Main extends React.Component {
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">About</h2>
           <span className="image main"><img src={pic03} alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+          <p>Sabina is a freelance digital artist based in The Cotswolds, UK, specialising in tattoo designs. Her work is often inspired by her love of nature.</p>
           {close}
         </article>
 
